6cd6eba17180af1215ae1e3288e04b80a07df856
不吃油炸鸡 <1425962791@qq.com>
2025-08-18 14:11:50 +0800

    add IRQ

--- Power_Pico/Core/Inc/stm32f4xx_it.h
+++ Power_Pico/Core/Inc/stm32f4xx_it.h
@@ -56,6 +56,7 @@ void DMA1_Stream4_IRQHandler(void);
 void TIM1_TRG_COM_TIM11_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
 void DMA2_Stream1_IRQHandler(void);
+void DMA2_Stream6_IRQHandler(void);
 void USART6_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
