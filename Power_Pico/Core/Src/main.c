e6e961c4a12df47a63e1432cc338608d907205c8
不吃油炸鸡 <1425962791@qq.com>
2025-12-06 16:38:01 +0800

    update

--- Power_Pico/Core/Src/main.c
+++ Power_Pico/Core/Src/main.c
@@ -75,7 +75,7 @@ int main(void)
 
   /* USER CODE BEGIN 1 */
   //this must set same as keil setting
-  SCB->VTOR = FLASH_BASE + 0x00000000U;
+  SCB->VTOR = FLASH_BASE + 0x0000C000U;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
