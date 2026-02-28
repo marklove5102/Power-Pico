1191bdeffb86c08cc3f305eecced89b73089e767
不吃油炸鸡 <1425962791@qq.com>
2026-02-28 19:56:06 +0800

    增加LV的缓存空间

--- software/Power_Pico/Middlewares/LVGL/porting/lv_port_disp.c
+++ software/Power_Pico/Middlewares/LVGL/porting/lv_port_disp.c
@@ -75,7 +75,7 @@ void lv_port_disp_init(void)
     /* Example 1
      * One buffer for partial rendering*/
     LV_ATTRIBUTE_MEM_ALIGN
-    static uint8_t buf_1_1[MY_DISP_HOR_RES * MY_DISP_VER_RES / 4 * BYTE_PER_PIXEL];
+    static uint8_t buf_1_1[MY_DISP_HOR_RES * MY_DISP_VER_RES / 2 * BYTE_PER_PIXEL];
     lv_display_set_buffers(disp, buf_1_1, NULL, sizeof(buf_1_1), LV_DISPLAY_RENDER_MODE_PARTIAL);
 #elif BUFFER_METHOD == 2
 
