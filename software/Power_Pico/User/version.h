36933acea9f7df48a527d80651c4ad096268ce24
不吃油炸鸡 <1425962791@qq.com>
2026-04-05 13:26:16 +0800

    加入信息

---
+++ software/Power_Pico/User/version.h
@@ -0,0 +1,62 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/***************************
+ * CURRENT VERSION OF Power-Pico
+ ***************************/
+/**
+ * total mechanical structure change will show in major
+ * hardware change will show in major
+ * software change will show in minor and patch
+ *
+ */
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 1
+#define VERSION_PATCH 0
+#define VERSION_INFO "added current range mode selection"
+
+/**
+ * No-Chicken的意思是no spring chicken any more...
+ *
+ */
+#define VERSION_AUTHOR_ENG_NAME    "No-Chicken"
+#define VERSION_AUTHOR_CN_NAME     "油炸鸡开源硬件"
+#define VERSION_PROJECT_LINK       "https://github.com/No-Chicken/Power-Pico"
+#define VERSION_HW_LINK            "https://oshwhub.com/no_chicken/powerpico"
+#define VERSION_BILIBILI_LINK      "https://space.bilibili.com/34154740"
+
+
+/**
+ * Wrapper functions for VERSION macros
+ */
+
+static inline int pp_version_major(void)
+{
+    return VERSION_MAJOR;
+}
+
+static inline int pp_version_minor(void)
+{
+    return VERSION_MINOR;
+}
+
+static inline int pp_version_patch(void)
+{
+    return VERSION_PATCH;
+}
+
+static inline const char *pp_version_info(void)
+{
+    return VERSION_INFO;
+}
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
